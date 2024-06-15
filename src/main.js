fb8c4db8656536efa890b6bfbd72d30d259f578b
Eduardo Link <edulink@gmail.com>
2024-06-15 10:27:16 -0300

    user info section, adjusting grid with placeholders

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import "./libs/dayjs.js"
 import "./styles/global.css"
 import "./styles/utils.css"
 import "./styles/header.css"
+import "./styles/user-info.css"
 import "./styles/user-profile.css"
+import "./styles/user-card.css"
+import "./styles/user-progress.css"
+import "./styles/user-history.css"
 
 // JS
